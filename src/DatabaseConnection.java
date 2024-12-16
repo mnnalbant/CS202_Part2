5753a93375716b977c60780f0aab44a47884978f
Ata Tan Dagidir <tan.dagidir@ozu.edu.tr>
2024-12-16 15:36:16 +0300

    Fix: forgotten changes from previous commit

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,11 +1,27 @@
 // DatabaseConnection.java
 import java.sql.*;
+import java.io.*;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/hotel_management";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1108";
-    
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        // Load configuration from properties file
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+            URL = properties.getProperty("db.url");
+            USER = properties.getProperty("db.username");
+            PASSWORD = properties.getProperty("db.password");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            // Handle the error appropriately (e.g., set default values or exit)
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
